d3e6b5c84ddab276699da3f8de9dbbcae14a97ef
olek007 <kelor2@o2.pl>
2014-07-12 13:36:15 +0200

    Update

---
+++ Assets/Scripts/PlayerEQ.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerEQ : MonoBehaviour {
+	
+	int buttonWidth = 
+	int bottonHeigh = 
+
+	void OnGUI()
+	{
+		GUI.Button(new Rect(
+	}
+}
