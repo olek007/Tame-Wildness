c72793083eb530cdd0d2c549ad6887bd3e432082
Adam Kokot <mujinn@interia.pl>
2014-07-12 12:59:08 +0200

    Update

--- Assets/Scripts/Power.cs
+++ Assets/Scripts/Power.cs
@@ -5,15 +5,29 @@ public class Power : MonoBehaviour {
 
 	public GameObject Player;
 
-	public void Force(GameObject ForceableItem)
+
+	public void Push()
 	{
 		Vector2 Dimension;
-		Dimension.x = ForceableItem.transform.position.x - Player.transform.position.x;
-		Dimension.y = ForceableItem.transform.position.y - Player.transform.position.y;
+		Dimension.x = gameObject.transform.position.x - Player.transform.position.x;
+		Dimension.y = gameObject.transform.position.y - Player.transform.position.y;
 		float Distance;
-		Distance = Vector2.Distance (ForceableItem.transform.position, Player.transform.position);
+		Distance = Vector2.Distance (gameObject.transform.position, Player.transform.position);
 		Dimension.x /= Distance;
 		Dimension.y /= Distance;
-		ForceableItem.rigidbody2D.AddForce (Dimension);
+		gameObject.rigidbody2D.AddForce (Dimension);
+	}
+
+	void OnMouseDown()
+	{
+		switch (pushForceLvl)
+		{
+			case 1:
+			{
+				Push(
+			}
+			break;
+		}
+
 	}
 }
