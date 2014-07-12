8bc2e828d0d63dd477f88a3a45a2ce6abc5a22b3
olek007 <kelor2@o2.pl>
2014-07-12 22:00:00 +0200

    Update map

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -13,7 +13,7 @@ public class CameraFollow : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		transform.position = new Vector3(player.transform.position.x, 0, -10);
+		transform.position = new Vector3(player.transform.position.x, 0, -8);
 		
 	}
 	
