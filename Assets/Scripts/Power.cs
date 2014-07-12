ecafdb4a342755a76d4883c1e6a38e00be9c7212
olek007 <kelor2@o2.pl>
2014-07-12 12:44:51 +0200

    Force Script

---
+++ Assets/Scripts/Power.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Power : MonoBehaviour {
+
+	public GameObject Player;
+
+	public void Force(GameObject ForceableItem)
+	{
+		Vector2 Dimension;
+		Dimension.x = ForceableItem.transform.position.x - Player.transform.position.x;
+		Dimension.y = ForceableItem.transform.position.y - Player.transform.position.y;
+		float Distance;
+		Distance = Vector2.Distance (ForceableItem.transform.position, Player.transform.position);
+		Dimension.x /= Distance;
+		Dimension.y /= Distance;
+		ForceableItem.rigidbody2D.AddForce (Dimension);
+	}
+}
