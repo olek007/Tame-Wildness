4758fe4148fbbeba95ddde67a9ab8046e40a38c2
Adam Kokot <mujinn@interia.pl>
2014-07-12 15:07:37 +0200

    Radar

--- Assets/Scripts/Power.cs
+++ Assets/Scripts/Power.cs
@@ -11,21 +11,9 @@ public class Power : MonoBehaviour {
 	private bool wasDragUsed = false;
 
 
-	void Boom()
+	void Start()
 	{
-		/*
-		Vector2 Dimension;
-		Dimension.x = gameObject.transform.position.x - Player.transform.position.x;
-		Dimension.y = gameObject.transform.position.y - Player.transform.position.y;
-		float Distance;
-		Distance = Vector2.Distance (gameObject.transform.position, Player.transform.position);
-		Dimension.x /= Distance;
-		Dimension.y /= Distance;
-		gameObject.rigidbody2D.AddForce (Dimension);
-		
-		*/
-		
-		
+		Radar.usable.Add(gameObject);
 	}
 	
 	void Push()
