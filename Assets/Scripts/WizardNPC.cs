08ec5d1eb48e4732ba12efdf98333ce43098e9f7
olek007 <kelor2@o2.pl>
2014-07-14 01:11:32 +0200

    Dialogi

--- Assets/Scripts/WizardNPC.cs
+++ Assets/Scripts/WizardNPC.cs
@@ -3,14 +3,16 @@ using System.Collections;
 
 public class WizardNPC : MonoBehaviour {
 
+	public Transform player;
 	private string[] dialogi = new string[5];
 	private bool isTalking = false;
 	private int dialogState = 0;
+	private int dialogWidth = 300;
+	private int dialogHeigh = 150;
 
 
-
-	void Start () {
-	
+	void Start ()
+	{
 		dialogi[0] = "Ach!  Nie spodziewalem sie, ze akurat Ty obudzisz w sobie tą moc.";
 		dialogi[1] = "Jaką moc? O czym Ty mówisz?";
 		dialogi[2] = "Nie mam zbyt wiele czasu. Nauczę Cię używać tej mocy, lecz nie licz na nic wielkiego.";
@@ -56,36 +58,37 @@ public class WizardNPC : MonoBehaviour {
 	{
 		if(isTalking)
 		{
+			GUI.color = Color.black;
+			GUI.skin.label.fontSize = 24;
 			switch (dialogState)
 			{
 				case 1:
 				{
-					GUI.Label(new Rect(250,250,100,100),dialogi[0]);
-					Debug.Log ("1");
+					GUI.Label(new Rect(Camera.current.WorldToScreenPoint(gameObject.transform.position).x-dialogWidth/2, Camera.current.WorldToScreenPoint(gameObject.transform.position).y - 150, dialogWidth, dialogHeigh), dialogi[0]);
 				}
 				break;
 				
 				case 2:
 				{
-					GUI.Label(new Rect(250,250,100,100),dialogi[1]);
+					GUI.Label(new Rect(Camera.current.WorldToScreenPoint(player.position).x - dialogWidth / 2, Camera.current.WorldToScreenPoint(player.position).y, dialogWidth, dialogHeigh), dialogi[1]);
 				}
 				break;
 				
 				case 3:
 				{
-					GUI.Label(new Rect(250,250,100,100),dialogi[2]);
+					GUI.Label(new Rect(Camera.current.WorldToScreenPoint(gameObject.transform.position).x - dialogWidth / 2, Camera.current.WorldToScreenPoint(gameObject.transform.position).y - 150, dialogWidth, dialogHeigh), dialogi[2]);
 				}
 				break;
 				
 				case 4:
 				{
-					GUI.Label(new Rect(250,250,100,100),dialogi[3]);
+					GUI.Label(new Rect(Camera.current.WorldToScreenPoint(player.position).x - dialogWidth / 2, Camera.current.WorldToScreenPoint(player.position).y, dialogWidth, dialogHeigh), dialogi[3]);
 				}
 				break;
 				
 				case 5:
 				{
-					GUI.Label(new Rect(250,250,100,100),dialogi[4]);
+					GUI.Label(new Rect(Camera.current.WorldToScreenPoint(gameObject.transform.position).x - dialogWidth / 2, Camera.current.WorldToScreenPoint(gameObject.transform.position).y - 150, dialogWidth, dialogHeigh), dialogi[4]);
 				}
 				break;
 			}	
